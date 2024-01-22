abbe5db5f61af85fe1504f994615289faa87382c
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-01-22 17:18:29 +0330

    add nav component

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <ul className="flex justify-center items-center gap-24 font-medium mt-16 text-2xl capitalize">
+      <li className="hover:text-blue duration-300 hover:text-3xl">About</li>
+      <li className="hover:text-blue duration-300 hover:text-3xl">Skills</li>
+      <li className="hover:text-blue duration-300 hover:text-3xl">Work</li>
+      <li className="hover:text-blue duration-300 hover:text-3xl">Contact</li>
+    </ul>
+  );
+}
+
+export default Navbar;
--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "800"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata = {

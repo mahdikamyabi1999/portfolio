81b196a7c496d69c2e6613999f53a4246d110cea
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-01-28 23:29:18 +0330

    make custom cursor invisible when mouse leave the page

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={poppins.className}>
         <Navbar />
-        <div className="cursor"></div>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,35 +1,48 @@
-'use client';
-import { useEffect } from "react";
+"use client";
+import { useEffect, useRef } from "react";
 import Contact from "./components/Contact/Contact";
 import Hero from "./components/Hero/Hero";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Works from "./components/works/Works";
 import Aos from "aos";
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
 
 export default function Home() {
-  
-  useEffect(()=> {
-    let mouseCursor = document.querySelector('.cursor');
-  window.addEventListener('mousemove',cursor);
-  function cursor(e) {
-    mouseCursor.style.top = e.pageY + "px";
-    mouseCursor.style.left = e.pageX + "px";
-  } 
+  const element = useRef(null);
+  useEffect(() => {
+    document.documentElement.addEventListener("mouseleave", () => {
+      return element.current.classList.add("hidden");
+    });
+    document.documentElement.addEventListener("mouseenter", () => {
+      return element.current.classList.remove("hidden");
+    });
+    let mouseCursor = document.querySelector(".cursor");
+    window.addEventListener("mousemove", cursor);
+
+    function cursor(e) {
+      mouseCursor.style.top = e.pageY + "px";
+      mouseCursor.style.left = e.pageX + "px";
+    }
     Aos.init({
       duration: 1000,
-      easing: 'ease-out',
-    })
-  },[])
+      easing: "ease-out",
+    });
+  }, []);
   return (
-    <main data-aos="fade-up"
-    data-aos-anchor-placement="top-bottom" className=" flex flex-col gap-y-32  md:my-0 md:mx-auto md:px-8 2xl:max-w-[1550px] md:max-w-[1300px]">
-      <Hero  />
-      <About />
-      <Skills />
-      <Works />
-      <Contact/>
-    </main>
+    <>
+      <div className="cursor" ref={element}></div>
+      <main
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+        className=" flex flex-col gap-y-32  md:my-0 md:mx-auto md:px-8 2xl:max-w-[1550px] md:max-w-[1300px]"
+      >
+        <Hero />
+        <About />
+        <Skills />
+        <Works />
+        <Contact />
+      </main>
+    </>
   );
 }

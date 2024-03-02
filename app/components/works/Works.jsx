e13325444d6cedfd41d825ea294bdb6be575a643
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-03-02 22:13:52 +0330

    add to more work sample

--- app/components/works/Works.jsx
+++ app/components/works/Works.jsx
@@ -7,6 +7,15 @@ import MobileWorkItem from "./MobileWorkItem";
 const works = [
   {
     id: 1,
+    src: "/camp.png",
+    h2: "camping App",
+    p: "fully responsive web app with Next js and Typescript",
+    span: "I work with Next js and Typescript",
+    demoLink: "https://campingapp.liara.run/",
+    githubLink: "https://github.com/mahdikamyabi1999/camping-app",
+  },
+  {
+    id: 2,
     src: "/shopping.png",
     h2: "eCommerce App",
     p: "eCommerce web application using React, Redux. in this App",
@@ -15,13 +24,22 @@ const works = [
     githubLink: "https://github.com/mahdikamyabi1999/Shopping-Cart",
   },
   {
-    id: 2,
+    id: 3,
     src: "/split.png",
     h2: "Split Bill App",
     p: "eCommerce web application using React, Redux. in this App",
     span: "I work with Redux toolkit and How to manage Data and Search with API",
     demoLink: "https://split.liara.run/",
     githubLink: "https://github.com/mahdikamyabi1999/Split-bill-mini-project",
+  },
+  {
+    id: 4,
+    src: "/weather.png",
+    h2: "Weather App",
+    p: "A simple, and responsive weather application with React",
+    span: "I work with API and How to manage Data and Search with that",
+    demoLink: "https://weatherapp.liara.run/",
+    githubLink: "https://github.com/mahdikamyabi1999/Weather-App",
   },
 ];
 
